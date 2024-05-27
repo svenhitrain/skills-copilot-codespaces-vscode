c3535b6cb2d132ba4dab91e4f4c74f6338aa13c5
Sven Hammesfahr <7516812+svenhitrain@users.noreply.github.com>
2024-05-27 14:24:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  // member.js
+  var skills = ['React', 'NodeJS', 'MongoDB', 'Express', 'Docker', 'Kubernetes', 'AWS', 'GCP'];
+  var member = 'John Doe';
+  console.log(member + ' has the following skills: ' + skills.join(', '));
+}
